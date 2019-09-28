9154947a3aa4e1f7b937452ff06c7ba4ffd4cfcd
JC Kim <jck@linecorp.com>
2019-09-28 23:17:21 +0900

    style: 💄 edit some docs

--- cmd/checkSpell.go
+++ cmd/checkSpell.go
@@ -1,4 +1,4 @@
-// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+// Copyright © 2019 JC Kim
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -51,14 +51,4 @@ var checkSpellCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(checkSpellCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// checkSpellCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// checkSpellCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
